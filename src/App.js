ceffb78683f2a091b44ee67d3d1dab989a6aea59
youzysu <111998760+youzysu@users.noreply.github.com>
2022-10-09 22:27:18 +0900

    card-event-mission

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Cards from "./cards.js";
 
 function App() {
+  const [cardList, setCardList] = useState();
+  const [selectedCard, setSelectedCard] = useState();
+  const [selectList, setSelectList] = useState([]);
+  const selectCard = () => {
+    setSelectedCard(cardList[Math.floor(Math.random() * cardList.length)]);
+    setCardList(cardList.filter((card) => card.name !== selectedCard.name))
+    setSelectList((pre) => [selectedCard, ...pre])
+  }
+  if (selectList.length === 3) {
+    window.alert(`이미 3명의 추첨을 완료했습니다. 당첨자는 ${selectList.map(({name}) => ([name]))}입니다.`);
+  };
+
+  useEffect(() => {
+    setCardList(Cards);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={selectCard}>추첨하기</button>
+      { selectedCard === undefined ? "" : (
+        <div>
+          <h3>{selectedCard.name}</h3>
+          <h4>{selectedCard.company} {selectedCard.team}</h4>
+          <h4>{selectedCard.phoneNumber} {selectedCard.email}</h4>
+        </div>
+      )}
     </div>
   );
 }
